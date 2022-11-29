a5fea1e826edfdee335770c92588af3b1045ba63
Adrian Danao <danao.adrian@ue.edu.ph>
2022-11-29 23:26:40 +0800

    Module Animations

--- app/src/main/java/com/example/testgame/MainActivity.java
+++ app/src/main/java/com/example/testgame/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.testgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnTest = findViewById(R.id.btnTest);
+        TextView txtTest = findViewById(R.id.txtTest);
+
+        TranslateAnimation animation = new TranslateAnimation(-200.0f, 200.0f, 0.0f, 0.0f);
+
+        animation.setDuration(2000);
+        animation.setRepeatCount(-1);
+        animation.setRepeatMode(2);
+
+        animation.setFillAfter(true);
+        btnTest.startAnimation(animation);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtTest.setText("The button works");
+            }
+        });
     }
 }
