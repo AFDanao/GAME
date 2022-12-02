a1e289d4db18c6bde5e6a73610f525385550b9e7
Adrian Danao <danao.adrian@ue.edu.ph>
2022-12-02 12:50:42 +0800

    Adrian

--- app/src/main/java/com/example/testgame/GameActivity1.java
+++ app/src/main/java/com/example/testgame/GameActivity1.java
@@ -12,5 +12,7 @@ public class GameActivity1 extends AppCompatActivity {
         setContentView(R.layout.activity_game1);
         //Alams pogi goat ll the time
         //Hotdog
+
+        //Adrian
     }
 }
