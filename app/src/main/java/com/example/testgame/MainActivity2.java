67b70f35f001b0924177e30e3258b1aa617e307b
Adrian Danao <danao.adrian@ue.edu.ph>
2022-12-02 11:48:44 +0800

    Made Activities for game 1 and game 2

--- app/src/main/java/com/example/testgame/MainActivity2.java
+++ app/src/main/java/com/example/testgame/MainActivity2.java
@@ -2,13 +2,33 @@ package com.example.testgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageButton btnGame1 = findViewById(R.id.btnGame1);
+        ImageButton btnGame2 = findViewById(R.id.btnGame2);
+
+        btnGame1.setOnClickListener(this);
+        btnGame2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnGame1) {
+            Intent game1 = new Intent(getApplicationContext(), GameActivity1.class);
+            startActivity(game1);
+        } else if (view.getId() == R.id.btnGame2) {
+            Intent game2 = new Intent(getApplicationContext(), GameActivity2.class);
+            startActivity(game2);
+        }
     }
 }
