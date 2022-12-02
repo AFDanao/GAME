4402a6cda1be362acdd68d4e222eb7fa5c3fe213
Adrian Danao <danao.adrian@ue.edu.ph>
2022-12-02 12:44:31 +0800

    Test

--- app/src/main/java/com/example/testgame/GameActivity1.java
+++ app/src/main/java/com/example/testgame/GameActivity1.java
@@ -10,5 +10,8 @@ public class GameActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
+
+
+        //Adrian
     }
 }
