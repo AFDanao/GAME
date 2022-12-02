32d84691bda9921a6d8c47d8d43d491159b3e81e
Alamani Ricardo <alamani.ricardojoseph@ue.edu.ph>
2022-12-02 12:16:16 +0800

    Alams Handsome

--- app/src/main/java/com/example/testgame/GameActivity1.java
+++ app/src/main/java/com/example/testgame/GameActivity1.java
@@ -10,5 +10,6 @@ public class GameActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
+        //Alams pogi goat ll the time
     }
 }
